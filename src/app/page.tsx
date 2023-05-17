5fbd93613e91814d9b5fc6024c02b7ce7da69571
maikel salles <maikelsalles@gmail.com>
2023-05-17 01:50:00 -0300

    nextjs implementation

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+import './globals.scss'
+
+export default function Home() {
+  return (
+    <>
+      <h1>UX UI Designer | Front End Developer | Accessibility Specialist</h1>
+      <p>
+          Hello!<br/>
+          I'm Maikel Salles, a Brazilian UI Front End Developer and UX Designer with 10+ years of experience, passionate about creating accessible user experiences while utilizing cutting-edge technologies. Proficient in building responsive designs using HTML5, CSS3, JavaScript, modern TypeScript frameworks, and many design tools. Strong critical thinking and self-motivated leader, familiar with agile environments and collaborating with multidisciplinary teams under pressure.
+      </p>
+      <a href="https://www.linkedin.com/in/maikelsalles" className="button iconLinkedIn" target="_blank">Follow me on LinkedIn</a>
+    </>
+  )
+}
