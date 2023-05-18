7605fea04871861a72bf81d7142946c2abd47957
maikel salles <maikelsalles@gmail.com>
2023-05-18 20:51:53 -0300

    fix: build

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link"
-  import styles from "./button.module.scss"
-  import globals from "../../globals.module.scss"
+import styles from "./button.module.scss"
+import globals from "../../globals.module.scss"
 
-export default function Button({ href, customClass, children }) {
+type ButtonProps = {
+  href: string;
+  customClass: string;
+  children: React.ReactNode;
+};
+
+export default function Button({ href, customClass, children }: ButtonProps) {
   if (typeof href === 'string' && (href.startsWith('http') || href.startsWith('https'))) {
     return (
       <a href={href} target="_blank" rel="noopener noreferrer" className={`${styles.button} ${globals[customClass]}`}>
@@ -10,13 +16,12 @@ export default function Button({ href, customClass, children }) {
       </a>
     );
   }
+
   return (
     <Link href={href} passHref>
-      <a className={`${styles.button} ${styles.customClass}`}>
-        {children} 
+      <a className={`${styles.button} ${globals[customClass]}`}>
+        {children}
       </a>
     </Link>
   );
 }
-
-// return <a href="https://www.linkedin.com/in/maikelsalles" className={`${styles.button} ${icons.icon}`} target="_blank">Follow me on LinkedIn</a>;
