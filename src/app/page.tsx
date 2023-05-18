ece859d617fc21af79cf348a769f4200c67f21c2
maikel salles <maikelsalles@gmail.com>
2023-05-18 00:14:42 -0300

    feat: about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/no-unescaped-entities */
 import './globals.scss'
 
+export const metadata = {
+  title: 'Maikel Salles - UX UI Designer | Front End Developer | Accessibility Specialist',
+  description: 'Personal website',
+}
+
 export default function Home() {
   return (
     <>
-      <h1>UX UI Designer | Front End Developer | Accessibility Specialist</h1>
+      <h1>UX UI Designer | Front End Developer<br/> Accessibility Specialist</h1>
       <p>
           Hello!<br/>
           I'm Maikel Salles, a Brazilian UI Front End Developer and UX Designer with 10+ years of experience, passionate about creating accessible user experiences while utilizing cutting-edge technologies. Proficient in building responsive designs using HTML5, CSS3, JavaScript, modern TypeScript frameworks, and many design tools. Strong critical thinking and self-motivated leader, familiar with agile environments and collaborating with multidisciplinary teams under pressure.
