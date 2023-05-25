1b90d4df9e49567fe7577e2b96b76b9871bf54d7
maikel salles <maikelsalles@gmail.com>
2023-05-25 00:11:19 -0300

    fix active menu

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -11,43 +11,41 @@ import globals from "../../globals.module.scss"
 import styles from "./header.module.scss"
 
 const navlinks = [
-    { href: "/about", label: "About" },
-    { href: "https://www.linkedin.com/in/maikelsalles/", title: "View Maikel Salles Resume on LinedIn", label: "Resume", target: "_blank" },
-    { href: "https://www.behance.net/maikelsalles", label: "Portfolio", target: "_blank" },
-    { href: "/contact", label: "Contact"}
-  ]
+  { href: "/about", label: "About" },
+  { href: "https://www.linkedin.com/in/maikelsalles/", title: "View Maikel Salles Resume on LinedIn", label: "Resume", target: "_blank" },
+  { href: "https://www.behance.net/maikelsalles", label: "Portfolio", target: "_blank" },
+  { href: "/contact", label: "Contact"}
+]
 
 export default function Header() {
-
-    const path = usePathname();
-    return (    
-        <header className={`${styles.header} ${globals.wrapper}`}>
-          <Link href="" title="Home page">
-            <picture>
-              <source srcSet={DarkLogo.src} media="(prefers-color-scheme: dark)" />
-              <Image
-                  src={LightLogo}
-                  alt="The Maikel Salles logo. It's the initials MS jointed on a white and curved line with a blue dot in the bottom right corner."
-                  width={80}
-                  height={80}
-              />
-            </picture>
-          </Link>
-          <nav className={styles.nav}>
-              { navlinks.map((link) => (
-                    <Link
-                    key={ link.href }
-                    href={ link.href }
-                    title={ link.title }
-                    target={ link.target }
-                    className={link.href === path ? 'active' : ''}
-                    >
-                    { link.label }
-                    </Link>
-                )
-
-              )}
-          </nav>
-        </header>
-    )
+  const path = usePathname();
+  return (    
+    <header className={`${styles.header} ${globals.wrapper}`}>
+      <Link href="" title="Home page">
+        <picture>
+          <source srcSet={DarkLogo.src} media="(prefers-color-scheme: dark)" />
+          <Image
+            src={LightLogo}
+            alt="The Maikel Salles logo. It's the initials MS jointed on a white and curved line with a blue dot in the bottom right corner."
+            width={80}
+            height={80}
+          />
+        </picture>
+      </Link>
+      <nav className={styles.nav}>
+        { navlinks.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              title={link.title}
+              target={link.target}
+              className={link.href === path ? styles.active : ''}
+            >
+              {link.label}
+            </Link>
+          )
+        )}
+      </nav>
+    </header>
+  )
 }
