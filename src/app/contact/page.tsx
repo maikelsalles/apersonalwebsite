a7acd3d0ede1b4069c282e4d9f69f38822f94640
Maikel Salles <maikelsalles@gmail.com>
2023-08-15 14:00:59 -0300

    Feat  phone number update (#19)
    * feat: mobile menu
    
    * feat: phone number update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -20,7 +20,7 @@ export default function Contact() {
                 <Button type="link" href="mailto:maikelsalles@gmail.com" title="Click to send an e-mail" icon="email" label="maikelsalles@gmail.com"/>
                 <Button type="link" href="https://www.linkedin.com/in/maikelsalles/" title="Follow me on LinkedIn" target="_blank" icon="linkedIn" label="LinkedIn"/>
                 <Button type="link" href="https://www.behance.net/maikelsalles" title="View my portfolio on Behance" target="_blank" icon="behance" label="Behance"/>
-                <Button type="link" href="calto:+5551996308777" title="Call me" icon="phone" label="+55 51 99630 8777"/>
+                <Button type="link" href="calto:+15878920290" title="Call me" icon="phone" label="+1 (587) 892 0290"/>
             </aside>
         </div>
     )
