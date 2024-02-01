3857705dfc49e9eacb848719ee3751f5f2af7b45
Maikel Salles <maikelsalles@gmail.com>
2024-02-01 00:36:09 -0300

    fix: light theme style (#22)
    Co-authored-by: maikel salles <maikel.salles@consulting-for.edenred.com>

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (      
         <footer className={`${styles.footer} ${globals.wrapper}`}>
           <small>
-              Imagem de <a href="https://pixabay.com/pt/users/19541953-19541953/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5971939" target="_blank">Ludovico Gianotti</a> por <a href="https://pixabay.com/pt//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5971939" target="_blank">Pixabay</a>
+              <a href="https://github.com/maikelsalles/personalwebsite" target="_blank" title="Project repository">About this project</a>
           </small>
         </footer>
     )
