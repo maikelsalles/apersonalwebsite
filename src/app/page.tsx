06bf175789c0c8f78d51b189b5adb7e115b2e6a4
maikel salles <maikel.salles@consulting-for.edenred.com>
2024-04-07 00:48:56 -0300

    fix: home module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Metadata } from 'next'
 import './globals.scss'
-import { Metadata } from 'next'
-import './globals.scss'
 import Button from '@/app/components/button'
 import styles from './home.module.scss'
 
 export const metadata: Metadata = {
-export const metadata: Metadata = {
   title: 'Maikel Salles - UX UI Designer | Front End Developer | Accessibility Specialist',
-  description: 'Hello world! This is my personal website',
   description: 'Hello world! This is my personal website',
 }
 
@@ -22,7 +18,6 @@ export default function Home() {
         Harmonizing design and code for elegant and inclusive experiences,<br/>
         where user needs are the cornerstone.
       </p>
-      <Button href="https://www.linkedin.com/in/maikelsalles" customClass="align-md-right" icon="linkedIn" target="_blank" label="Follow me on LinkedIn"/>
       <Button href="https://www.linkedin.com/in/maikelsalles" customClass="align-md-right" icon="linkedIn" target="_blank" label="Follow me on LinkedIn"/>
     </section>
   )
