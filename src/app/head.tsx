e78a182c1fca366027f865d03753ceebb39da9ab
Maikel Salles <maikelsalles@gmail.com>
2025-02-12 17:19:31 -0700

    fix: og image

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,6 +5,7 @@ export default function Head(){
             <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
             <meta name='viewport' content='width=device-width, initial-scale=1'/>
             <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+            <meta property="og:image" content="@img/maikel-salles-w.jpg" />
         </>
     )
 }
